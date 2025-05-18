19408cecf1ed3a5757cb5e3750f7358965941f20
Erdem Kaya <kerdem@live.com>
2025-05-18 15:45:01 +0200

    grpc test for accountservice

---
+++ Presentation/Services/VerificationServiceImplemation.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using VerificationServiceGrpcServer;
+
+namespace Presentation.Services;
+
+public class VerificationServiceImplemation : VerificationServiceProto.VerificationServiceProtoBase
+{
+    public override Task<ValidateEmailResponse> ValidateEmail(ValidateEmailRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new ValidateEmailResponse
+        {
+            Success = true,
+            Message = "Email validated successfully",
+            Email = request.Email
+        });
+    }
+
+    public override Task<ConfirmUserEmailResponse> ConfirmUserEmail(ConfirmUserEmailRequest request, ServerCallContext context)
+    {
+        return Task.FromResult(new ConfirmUserEmailResponse
+        {
+            Success = true,
+            Message = "Email confirmed successfully"
+        });
+    }
+}
+
